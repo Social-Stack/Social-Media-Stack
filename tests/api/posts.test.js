572d5afb6fccfc5ff355593f6a5a667b556e5c25
nadiadubell <ndubell01@gmail.com>
2022-10-26 13:32:35 -0500

    started writing api posts tests

---
+++ tests/api/posts.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+
+describe("api/posts", () => {
+  describe("POST api/posts/new", () => {
+    it("Makes a new post if a user is logged in", async () => {
+      //need to login user?
+    });
+  });
+});
