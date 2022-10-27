8811e6affa439c83621c069483b1a1ece8aa8132
Fred Davis <F092495@gmail.com>
2022-10-27 01:31:07 -0400

    missed token in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const Header = ({ loggedIn, username }) => {
+const Header = ({ loggedIn, username, setToken }) => {
   let navigate = useNavigate();
   const logout = () => {
     navigate("/login");
