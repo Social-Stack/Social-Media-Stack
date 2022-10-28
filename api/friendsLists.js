2c79961c82de6e95075c96087a1d0282fafc4383
nadiadubell <ndubell01@gmail.com>
2022-10-28 14:13:53 -0500

    api friendsList functions built out

--- api/friendsLists.js
+++ api/friendsLists.js
@@ -1,8 +1,12 @@
-//friendsLists file - delete this if you're writing this file
 const express = require("express");
 const friendsRouter = express.Router();
 
-const { addFriends, getUserByUsername, removeFriend } = require("../db");
+const {
+  addFriends,
+  getUserByUsername,
+  removeFriend,
+  getFriendsByUserId,
+} = require("../db");
 
 const { requireUser } = require("./utils");
 
@@ -35,9 +39,9 @@ friendsRouter.delete(
       const { id: userId } = req.user;
       const { id: friendsId } = await getUserByUsername(username);
 
-      const removeFriend = await removeFriend(userId, friendsId);
+      const deletedFriend = await removeFriend(userId, friendsId);
       res.send({
-        removeFriend,
+        deletedFriend,
         success: "You've successfully removed a friend",
       });
     } catch ({ error, message }) {
@@ -46,11 +50,22 @@ friendsRouter.delete(
   }
 );
 
-friendsRouter.get("/friends/:friendId", requireUser, async (req, res, next) => {
+friendsRouter.get("/friends", requireUser, async (req, res, next) => {
   try {
-    const { username } = req.params;
     const { id: userId } = req.user;
-    const { id: friendId } = await getUserByUsername(username);
+
+    const friends = await getFriendsByUserId(userId);
+    if (friends) {
+      res.send({
+        friends,
+        success: "Your friends were successfully retrieved!",
+      });
+    } else {
+      next({
+        error: "YouHaveNoFriendsError",
+        message: "You currently have no friends. Please add some.",
+      });
+    }
   } catch ({ error, message }) {
     next({ error, message });
   }
