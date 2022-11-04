325933a95f5db87f70ca5430b2594a0390792a61
nadiadubell <ndubell01@gmail.com>
2022-11-04 12:42:48 -0500

    mobile header css finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../stylesheets/Header.css";
 
 const Header = ({ setLoggedIn, loggedIn, setUsername, username, setToken }) => {
   // const navigate = useNavigate();
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("username");
@@ -15,6 +16,10 @@ const Header = ({ setLoggedIn, loggedIn, setUsername, username, setToken }) => {
   };
   const picture = localStorage.getItem("profile pic");
 
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
   return (
     <div className="header">
       <Link id="logo" to="/newsfeed">
@@ -23,13 +28,13 @@ const Header = ({ setLoggedIn, loggedIn, setUsername, username, setToken }) => {
       {loggedIn ? (
         <div>
           <nav id="header-links">
+            <img id="profile-pic" src={picture} />
             {/* <Link to="/profile">Profile</Link> | {""} */}
             {/* {isAdmin ? <Link to="admin">Admin | {""}</Link> : null} */}
             {/* <div> */}
             {/* <img id="profile-pic" src={require(`./Assets/defaultPic.png`)} /> offline mode for Fred in the sky */}
-            <img id="profile-pic" src={picture} />
             {/* </div> */}
-            <Link to="/messages">Messages </Link> |{" "}
+            <Link to="/messages">Messages </Link>
             <Link to="/login" onClick={logout}>
               Logout {username}
             </Link>
@@ -38,10 +43,122 @@ const Header = ({ setLoggedIn, loggedIn, setUsername, username, setToken }) => {
       ) : (
         <div>
           <nav id="header-links">
-            <Link to="/login">Login/Register</Link> |
+            <Link to="/login">Login/Register</Link>
           </nav>
         </div>
       )}
+      <div onClick={toggleHamburger} className="hamburger">
+        <div className="burger burger1" />
+        <div className="burger burger2" />
+        <div className="burger burger3" />
+      </div>
+
+      <style jsx>
+        {`
+          @import url("https://fonts.googleapis.com/css2?family=Barlow&family=Poppins:wght@600&display=swap");
+
+          #profile-pic {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            margin-right: 5px;
+          }
+          
+          .header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            /* background-color: rgb(240, 3, 3); */
+            height: 10%;
+            /* text-decoration: none; */
+          }
+          
+          #header-links {
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            color: black;
+            font-family: "Barlow", sans-serif;
+          }
+          
+          #header-links:last-child {
+            padding-right: 10px;
+          }
+          
+          #header-links a {
+            text-decoration: none;
+            color: black;
+            padding: 0px 5px;
+          }
+          
+          #logo {
+            padding-left: 10px;
+            text-decoration: none;
+            color: black;
+            font-family: "Poppins", sans-serif;
+            font-weight: bold;
+            font-size: 20px;
+          }
+          
+          .hamburger {
+            display: none;
+            width: 2rem;
+            height: 3rem;
+            padding-top: 10px;
+            margin-right: 10px;
+            /* justify-content: space-around; */
+            flex-flow: column nowrap;
+            z-index: 10;
+          }
+          
+          .burger {
+            width: 2rem;
+            height: 0.25rem;
+            border-radius: 10px;
+            background-color: black;
+            margin-bottom: 10px;
+            transform-origin: 1px;
+            transition: all 0.3s linear;
+          }
+
+          .burger1 {
+            transform: ${hamburgerOpen ? "rotate(45deg)" : "rotate(0)"};
+          }
+
+          .burger2 {
+            transform: ${hamburgerOpen ? "translateX(100%)" : "translateX(0)"};
+            opacity: ${hamburgerOpen ? 0 : 1};
+          }
+          
+          .burger3 {
+            transform: ${hamburgerOpen ? "rotate(-45deg)" : "rotate(0)"};
+          }
+
+          @media (max-width: 768px) {
+            .hamburger {
+              display: block;
+              padding-top: 20px;
+              margin-left: 10px;
+              z-index: 6;
+            }
+            
+            #header-links {
+              display: ${hamburgerOpen ? "inline-flex;" : "none;"}
+              background-color: rgb(245, 244, 244);
+              height: 55vh;
+              width: 55vw;
+              margin-top: 20px;
+              padding-top: 10px;
+              padding-bottom: 5px;
+              padding-left: 10px;
+              position: fixed;
+              flex-direction: column;
+              justify-content: flex-start;
+              align-items: flex-start;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
