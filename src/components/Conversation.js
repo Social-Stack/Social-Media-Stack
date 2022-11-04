69276211b9db03aff93644f1775368ee77357ec7
Sang <sangalee92@gmail.com>
2022-11-04 02:42:15 -0500

    successfully shows only one conversation at a time on click

---
+++ src/components/Conversation.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { getFriendMessages } from "../api";
+
+//IGNORE EVERYTHING IN THIS FILE, IT HASN'T BEEN IMPLEMENTED YET
+const Conversation = (props) => {
+  return (
+    <div>
+      <div>Testing</div>
+    </div>
+  );
+};
+
+export default Conversation;
