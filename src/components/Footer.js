5f2d2e081681bb7dae1c1febadb1891d2865e452
nadiadubell <ndubell01@gmail.com>
2022-11-04 18:25:42 -0500

    added footer component and css

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import "../stylesheets/Footer.css";
+
+const Footer = () => {
+  return (
+    <div id="footer-container">
+      <div id="footer-items">
+        <h3 id="footer-copyright">&#169; 2022 Team Sequence</h3>
+        <p id="footer-text">For demostration purposes only</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
