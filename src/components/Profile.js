b029b2102080b7afd3cf8ac913a9a8f0c86c407f
nadiadubell <ndubell01@gmail.com>
2022-11-07 14:11:22 -0600

    working on building profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
-import { getMyUserInfo, getPostsByUserId } from "../api";
+import { getMyFriends, getMyUserInfo, getPostsByUserId } from "../api";
+import timeAgo from "node-time-ago";
+import SinglePost from "./SinglePost";
 
 const Profile = (props) => {
   const [userInfo, setUserInfo] = useState({});
   const [userPosts, setUserPosts] = useState([]);
+  const [userFriends, setUserFriends] = useState([]);
   const token = localStorage.getItem("token");
+
   useEffect(() => {
     const getUserInfo = async () => {
       const info = await getMyUserInfo(token);
       const { id } = await getMyUserInfo(token);
       const posts = await getPostsByUserId(token, id);
+      const friends = await getMyFriends(token, id);
       console.log("posts", posts);
       setUserInfo(info);
       setUserPosts(posts);
+      setUserFriends(friends.friendsLists);
     };
     getUserInfo();
   }, []);
@@ -22,14 +28,20 @@ const Profile = (props) => {
       <h1>
         {userInfo.firstname} {userInfo.lastname}
       </h1>
-      {userPosts[0] &&
+      <p>{String(userFriends.length)} friends</p>
+      {/* {userPosts[0] &&
         userPosts.map((post, i) => {
           return (
             <div>
+              <img height="80px" src={post.profilePic} />
+              <h3>
+                {userInfo.firstname} {userInfo.lastname} {timeAgo(post.time)}
+              </h3>
               <p>{post.text}</p>
             </div>
           );
-        })}
+        })} */}
+      <SinglePost />
     </div>
   );
 };
