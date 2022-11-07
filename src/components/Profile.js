f059a92d4839020fe925ecfccf6042a58fb42880
nadiadubell <ndubell01@gmail.com>
2022-11-07 15:14:13 -0600

    added singlepost and newpost components into profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { getMyFriends, getMyUserInfo, getPostsByUserId } from "../api";
-import timeAgo from "node-time-ago";
 import SinglePost from "./SinglePost";
+import NewPost from "./NewPost";
+import { Link } from "react-router-dom";
 
 const Profile = (props) => {
   const [userInfo, setUserInfo] = useState({});
   const [userPosts, setUserPosts] = useState([]);
   const [userFriends, setUserFriends] = useState([]);
+  const [loadingTrigger, setLoadingTrigger] = useState(true);
   const token = localStorage.getItem("token");
 
   useEffect(() => {
@@ -23,25 +25,38 @@ const Profile = (props) => {
     getUserInfo();
   }, []);
   return (
-    <div>
-      <img id="user-image" height="150px" src={userInfo.picUrl} />
-      <h1>
-        {userInfo.firstname} {userInfo.lastname}
-      </h1>
-      <p>{String(userFriends.length)} friends</p>
-      {/* {userPosts[0] &&
-        userPosts.map((post, i) => {
+    <div id="profile-container">
+      <div id="userinfo-container">
+        <img id="user-image" height="150px" src={userInfo.picUrl} />
+        <h1>
+          {userInfo.firstname} {userInfo.lastname}
+        </h1>
+        <p>{String(userFriends.length)} friends</p>
+        <Link to="/friendslists">
+          <button>friends</button>
+        </Link>
+      </div>
+      <div id="friends-container">
+        {userFriends.map((friend, i) => {
           return (
             <div>
-              <img height="80px" src={post.profilePic} />
-              <h3>
-                {userInfo.firstname} {userInfo.lastname} {timeAgo(post.time)}
-              </h3>
-              <p>{post.text}</p>
+              <img height="80px" src={friend.picUrl} />
+              <h3>{friend.firstname}</h3>
             </div>
           );
-        })} */}
-      <SinglePost />
+        })}
+      </div>
+      <NewPost
+        token={token}
+        loadingTrigger={loadingTrigger}
+        setLoadingTrigger={setLoadingTrigger}
+      />
+      <div id="profile-posts-container">
+        {userPosts[0] &&
+          userPosts.map((post, i) => {
+            return <SinglePost key={i} post={post} token={token} />;
+          })}
+      </div>
     </div>
   );
 };
