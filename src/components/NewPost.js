1f0ddfd3843cfef31c3e6ded99e9bba6848d81db
CJ <cjfritz9@gmail.com>
2022-11-08 12:35:59 -0700

    css for new post box

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -17,25 +17,27 @@ const NewPost = ({token, loadingTrigger, setLoadingTrigger}) => {
         setVisibility(newVis)
     }
 
-    //dont mind the bad style just focusing on functionality
-
     return (
-      <div id='new-post-wrapper'>
-        <div id="newPostBox">
-            <form id='new-post-form' onSubmit={(event) => {handleSubmit(event)}}>
-                <textarea id="newPostTextBox" type='text' placeholder="What's on your mind?" value={text} onChange={(event) => setText(event.target.value)}/>
-                <div id="newPostBottomBar">
-                    <label id="post-visibility-text">Visibility: </label>
-                    <select
-                        value={visibility}
-                        onChange={(event) => {handleVisibility(event.target.value)}}>
-                            <option value='friends'>Friends Only</option>
-                            <option value='public'>Public</option>
-                    </select>
-                    <button type="submit" id="postButton">Post!</button>
-                </div>
-            </form>
+      <div id="newsfeed-upper-wrapper">
+        <div id='new-post-wrapper'>
+          <div id="newPostBox">
+              <form id='new-post-form' onSubmit={(event) => {handleSubmit(event)}}>
+                  <textarea id="newPostTextBox" type='text' placeholder="What's on your mind?" value={text} onChange={(event) => setText(event.target.value)}/>
+                  <div id="newPostBottomBar">
+                      <label id="post-visibility-text">Visibility: </label>
+                      <select
+                          id="post-visibility-selector"
+                          // value={visibility}
+                          onChange={(event) => {handleVisibility(event.target.value)}}>
+                              <option value='friends'>Friends Only</option>
+                              <option value='public'>Public</option>
+                      </select>
+                      <button type="submit" id="post-button">Post!</button>
+                  </div>
+              </form>
+          </div>
         </div>
+        <div id="spacer-div"></div>
       </div>
     )
 }
