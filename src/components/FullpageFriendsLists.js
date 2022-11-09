6b1db5a6bb813462210f1aa1328c9f9748f0328d
nadiadubell <ndubell01@gmail.com>
2022-11-09 11:46:49 -0600

    fixed bug so now when you click on the friends button it displays the correct info

--- src/components/FullpageFriendsLists.js
+++ src/components/FullpageFriendsLists.js
@@ -1,19 +1,18 @@
 import { useEffect, useState } from "react";
-import { getMyFriends, getMyUserInfo } from "../api";
-import { Link } from "react-router-dom";
+import { getMyFriends, getProfileData } from "../api";
+import { Link, useParams } from "react-router-dom";
 import "../stylesheets/FullpageFriendsLists.css";
-import { Profile } from ".";
 
-const FullpageFriendsLists = (props) => {
+const FullpageFriendsLists = () => {
   const [friends, setFriends] = useState([]);
 
   const token = localStorage.getItem("token");
 
+  const { username } = useParams();
   useEffect(() => {
     const getAllFriends = async () => {
-      const { id } = await getMyUserInfo(token);
-      const myFriends = await getMyFriends(token, id);
-      console.log(myFriends);
+      const myInfo = await getProfileData(token, username);
+      const myFriends = await getMyFriends(token, myInfo.user.id);
       setFriends(myFriends.friendsLists);
     };
     getAllFriends();
@@ -23,27 +22,28 @@ const FullpageFriendsLists = (props) => {
     <div>
       <h1 id="friendslists-page-title">My Friends</h1>
       <div id="friends-container">
-        {friends.map((friend, i) => {
-          return (
-            <div id="friend-list" key={i}>
-              <Link to={`/profile/${friend.username}`}>
-                <img id="friend-img" height="80px" src={friend.picUrl} />
-              </Link>
-              <h3 id="friend-name">
+        {friends[0] &&
+          friends.map((friend, i) => {
+            return (
+              <div id="friend-list" key={i}>
                 <Link to={`/profile/${friend.username}`}>
-                  {friend.firstname} {friend.lastname}
+                  <img id="friend-img" height="80px" src={friend.picUrl} />
                 </Link>
-              </h3>
-              <button
-                onClick={() => {
-                  console.log(friend);
-                }}
-              >
-                Helper
-              </button>
-            </div>
-          );
-        })}
+                <h3 id="friend-name">
+                  <Link to={`/profile/${friend.username}`}>
+                    {friend.firstname} {friend.lastname}
+                  </Link>
+                </h3>
+                <button
+                  onClick={() => {
+                    console.log(friend);
+                  }}
+                >
+                  Helper
+                </button>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
