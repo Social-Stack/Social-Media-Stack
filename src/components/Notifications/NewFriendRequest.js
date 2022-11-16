28425c09ba393701b5888452e91ac7c33a9fc476
nadiadubell <ndubell01@gmail.com>
2022-11-16 00:06:53 -0600

    css for notifications done

--- src/components/Notifications/NewFriendRequest.js
+++ src/components/Notifications/NewFriendRequest.js
@@ -1,33 +1,55 @@
 import React from "react";
 import { acceptFriend, denyFriend } from "../../api";
 
-const NewFriendRequest = ({token, notification, notiTrigger, setNotiTrigger}) => {
-
-
-    const acceptFriendButton = async() => {
-        const friend = await acceptFriend(token, notification.miscId, notification.id);
-        if(friend.success){
-            setNotiTrigger(!notiTrigger);
-        }
-    };
-
-    const denyFriendButton = async() => {
-        await denyFriend(token, notification.miscId, notification.id);
-        setNotiTrigger(!notiTrigger);
-    };
-
-    const tempStyle = {
-        height:'50px'
+const NewFriendRequest = ({
+  token,
+  notification,
+  notiTrigger,
+  setNotiTrigger,
+}) => {
+  const acceptFriendButton = async () => {
+    const friend = await acceptFriend(
+      token,
+      notification.miscId,
+      notification.id
+    );
+    if (friend.success) {
+      setNotiTrigger(!notiTrigger);
     }
-    return (
-        <div style={tempStyle}>
-            {notification.text}
-            <button onClick={() => {
-                acceptFriendButton()
-            }}>Accept</button>
-            <button onClick={()=>{denyFriendButton()}}>Delete</button>
-        </div>
-    )
-}
+  };
+
+  const denyFriendButton = async () => {
+    await denyFriend(token, notification.miscId, notification.id);
+    setNotiTrigger(!notiTrigger);
+  };
+
+  const tempStyle = {
+    height: "50px",
+  };
+
+  return (
+    <div id="friend-request">
+      {notification.text}
+      <div id="notification-btns">
+        <button
+          id="accept-btn"
+          onClick={() => {
+            acceptFriendButton();
+          }}
+        >
+          Accept
+        </button>
+        <button
+          id="decline-btn"
+          onClick={() => {
+            denyFriendButton();
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default NewFriendRequest;
--- src/components/Notifications/NotificationPage.js
+++ src/components/Notifications/NotificationPage.js
@@ -1,43 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { getAllMyNotifications, seeMyNotifications } from "../../api";
 import NotificationItemHandler from "./NotificationItemHandler";
+import "../../stylesheets/NotificationPage.css";
 
+const NotificationPage = ({ token }) => {
+  const [notifications, setNotifications] = useState([]);
+  const [notiTrigger, setNotiTrigger] = useState(false);
 
-const NotificationPage = ({token}) => {
-    const [notifications, setNotifications] = useState([]);
-    const [notiTrigger, setNotiTrigger] = useState(false);
+  const fetchNotifications = async () => {
+    const notis = await getAllMyNotifications(token);
+    setNotifications(notis);
+  };
 
-    const fetchNotifications = async() => {
-        const notis = await getAllMyNotifications(token);
-        setNotifications(notis);
-    }
+  const seeAllNotis = async () => {
+    await seeMyNotifications(token);
+  };
 
-    const seeAllNotis = async() => {
-        await seeMyNotifications(token);
-    }
+  useEffect(() => {
+    fetchNotifications();
+    setTimeout(seeAllNotis, 500);
+  }, [notiTrigger]);
 
-    useEffect(() => {
-        fetchNotifications();
-        setTimeout(seeAllNotis, 500);
-    },[notiTrigger])
-
-    return (
-        <div>
-            <h4>Notifications</h4>
-            {notifications.length ? 
-            notifications.map((notification) => {
-                return (
-                    <NotificationItemHandler
-                    setNotiTrigger={setNotiTrigger}
-                    notiTrigger={notiTrigger}
-                    key={notification.id}
-                    token={token}
-                    notification={notification}/>
-                )
-            })
-            : <> no notifications</>
-            }
-        </div>
-    )
-}
-export default NotificationPage
+  return (
+    <div id="notifications-wrapper">
+      <h2 id="notifications-title">Notifications</h2>
+      {notifications.length ? (
+        notifications.map((notification) => {
+          return (
+            <NotificationItemHandler
+              setNotiTrigger={setNotiTrigger}
+              notiTrigger={notiTrigger}
+              key={notification.id}
+              token={token}
+              notification={notification}
+            />
+          );
+        })
+      ) : (
+        <> no notifications</>
+      )}
+    </div>
+  );
+};
+export default NotificationPage;
