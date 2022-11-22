9d291af609bb48d6fbbc1c9b55b7572588864026
nadiadubell <ndubell01@gmail.com>
2022-11-21 19:22:34 -0600

    added className styling based off if you're on newsfeed or on profile, removed spacer div from new post and moved it to newsfeed

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { getAllPublicPosts, getMyUserInfo, getMyFriends, getNewsFeed } from "../api";
+import {
+  getAllPublicPosts,
+  getMyUserInfo,
+  getMyFriends,
+  getNewsFeed,
+} from "../api";
 import NewPost from "./NewPost";
 import SinglePost from "./SinglePost";
 import "../stylesheets/NewsFeed.css";
@@ -9,17 +14,19 @@ const NewsFeed = ({ token }) => {
   const [allPosts, setAllPosts] = useState([]);
   const [loadingTrigger, setLoadingTrigger] = useState(true);
   const [friends, setFriends] = useState([]);
+  const [width, setWidth] = useState("");
 
   useEffect(() => {
+    setWidth("newsfeed-NewPostBox");
     fetchPosts();
     getAllFriends();
     setInterval(getAllFriends, 30000);
   }, [loadingTrigger, token]);
-  
+
   const getAllFriends = async () => {
     const { id } = await getMyUserInfo(token);
     const myFriends = await getMyFriends(token, id);
-    console.log(myFriends)
+    console.log(myFriends);
     setFriends(myFriends.friendsLists);
   };
 
@@ -34,11 +41,15 @@ const NewsFeed = ({ token }) => {
   return (
     <>
       {/* <button onClick={() => helpFunction()}>helper</button> */}
-      <NewPost
-        token={token}
-        loadingTrigger={loadingTrigger}
-        setLoadingTrigger={setLoadingTrigger}
-      />
+      <div id="newsfeed-upper-wrapper">
+        <NewPost
+          token={token}
+          loadingTrigger={loadingTrigger}
+          setLoadingTrigger={setLoadingTrigger}
+          width={width}
+        />
+        <div id="spacer-div"></div>
+      </div>
       <div id="main-content-container">
         <div id="newsfeed-container">
           <div id="posts-container">
@@ -55,20 +66,23 @@ const NewsFeed = ({ token }) => {
             <div id="side-panel-friends">
               {friends
                 ? friends.map((friend) => {
-                // return (
-                //   <div id="side-panel-friend" key={i}>
-                //     <img id="friend-img" height="50px" src={friend.picUrl} />
-                //     <div>
-                //       <p>{friend.username}</p>
-                //       <p>{timeAgo(friend.lastActive)}</p>
-                //     </div>
-                //   </div>
-                // );
-                return(
-                <FriendPanel key={friend.id}
-                friend={friend}></FriendPanel>)
-                })
-              : null}
+                    // return (
+                    //   <div id="side-panel-friend" key={i}>
+                    //     <img id="friend-img" height="50px" src={friend.picUrl} />
+                    //     <div>
+                    //       <p>{friend.username}</p>
+                    //       <p>{timeAgo(friend.lastActive)}</p>
+                    //     </div>
+                    //   </div>
+                    // );
+                    return (
+                      <FriendPanel
+                        key={friend.id}
+                        friend={friend}
+                      ></FriendPanel>
+                    );
+                  })
+                : null}
             </div>
           </div>
         </div>
