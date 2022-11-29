ee020cd8779b02e126a7025df18e89c7e02a7aa4
Fred Davis <F092495@gmail.com>
2022-11-29 14:11:27 -0500

    friends panel online status now refreshes every 30 seconds

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -12,30 +12,30 @@ import { NewsFeedLeftPanel } from './NewsFeedLeftPanel';
 import FriendPanel from './NewsFeedFriendPanel';
 import '../stylesheets/NewsFeed.css';
 
-const NewsFeed = ({ token }) => {
+const NewsFeed = () => {
   const [allPosts, setAllPosts] = useState([]);
   const [loadingTrigger, setLoadingTrigger] = useState(true);
   const [friends, setFriends] = useState([]);
   const [width, setWidth] = useState('');
-  const [friendsTrigger, setFriendsTrigger] = useState(false);
-
-  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     setWidth('newsfeed-NewPostBox');
     fetchPosts();
     getAllFriends();
-    setInterval(getAllFriends, 30000);
   }, [loadingTrigger, token]);
 
+
+  useEffect(()=>{
+    setInterval(getAllFriends, 3000)
+  },[])
+
   const getAllFriends = async () => {
+    if(token){
     const { id } = await getMyUserInfo(token);
     const myFriends = await getMyFriends(token, id);
-
-    console.log(myFriends);
-
     setFriends(myFriends.friendsLists);
-    setFriendsTrigger(!friendsTrigger);
+    }
   };
 
   const fetchPosts = async () => {
@@ -43,7 +43,8 @@ const NewsFeed = ({ token }) => {
     setAllPosts(feed);
   };
   // const helpFunction = async() => {
-  //   console.log(await getNewsFeed(token))
+  //   console.log(friendsTrigger)
+  //   console.log(token)
   // }
 
   return (
