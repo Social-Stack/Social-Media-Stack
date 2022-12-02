3b0697c605da1480cd9f753b6cf8e16e822b33d8
CJ <cjfritz9@gmail.com>
2022-12-02 13:42:00 -0700

    further db testing

--- app.js
+++ app.js
@@ -1,74 +1,74 @@
-require("dotenv").config();
-const client = require("./db/client");
-const cors = require("cors");
-const path = require("path");
-const http = require("http");
-const chalk = require("chalk");
+require('dotenv').config();
+const client = require('./db/client');
+const cors = require('cors');
+const path = require('path');
+const http = require('http');
+const chalk = require('chalk');
 
 // const favicon = require("serve-favicon");
 
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-const morgan = require("morgan");
-app.use(morgan("dev"));
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: '*'
   })
 );
 
 app.use((req, res, next) => {
-  console.log(chalk.whiteBright("<___Body Logger Start___>"));
+  console.log(chalk.whiteBright('<___Body Logger Start___>'));
   console.log(req.body);
-  console.log(chalk.whiteBright("<___Body Logger End___>"));
+  console.log(chalk.whiteBright('<___Body Logger End___>'));
 
   next();
 });
 
-const apiRouter = require("./api");
-const { rebuildDB } = require("./db/seedData");
-app.use("/api", apiRouter);
+const apiRouter = require('./api');
+const { rebuildDB } = require('./db/seedData');
+app.use('/api', apiRouter);
 
-app.get("*", async (req, res, next) => {
+app.get('*', async (req, res, next) => {
   res.status(404);
   next({
-    error: "Page not found",
-    message: "The page you are looking for was not found",
+    error: 'Page not found',
+    message: 'The page you are looking for was not found'
   });
 });
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.use((error, req, res, next) => {
   res.send({
     error: error.error,
-    message: error.message,
+    message: error.message
   });
 });
 
-const init = async () => {
-  const connection = await client.connect();
-  console.log("DB CONNECTION MONITOR", connection)
-  const PORT = process.env["PORT"] ?? 4000;
-  const server = http.createServer(app);
-  // await rebuildDB();
+// const init = async () => {
+// const connection = await client.connect();
+// console.log("DB CONNECTION MONITOR", connection)
+const PORT = process.env['PORT'] ?? 4000;
+const server = http.createServer(app);
+// await rebuildDB();
 
-  server.listen(PORT, () => {
-    console.log(
-      chalk.blueBright("Server is listening on"),
-      chalk.bold.yellowBright("PORT :", PORT),
-      chalk.blueBright(".Concat with Social Stack!")
-    );
-  });
-};
+server.listen(PORT, () => {
+  console.log(
+    chalk.blueBright('Server is listening on'),
+    chalk.bold.yellowBright('PORT :', PORT),
+    chalk.blueBright('.Concat with Social Stack!')
+  );
+});
+// };
 
-init();
+// init();
 
 module.exports = app;
